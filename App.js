afc890ab09f0381d53da834982de55c76ea4c820
ibrahimozdogan <halil.ozdogan@useinsider.com>
2020-05-09 16:11:23 +0300

    styling fixes

--- App.js
+++ App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Background from './src/Components/Background';
 
-export default function App() {
-  return (
-    <Background>
-      <View style={styles.container}>
-        <Text>Gradient example in react-native-svg!</Text>
-      </View>
-    </Background>
-  );
+export default function App () {
+    return (
+        <Background>
+            <View style={styles.container}>
+                <Text>Gradient example in react-native-svg!</Text>
+            </View>
+        </Background>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
